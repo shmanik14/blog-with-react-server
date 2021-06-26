26225f704519e7ddd0cb94150f7b196168afa923
sahadat <shmanik14@yahoo.com>
2021-06-26 08:04:42 +0600

    add api

--- index.js
+++ index.js
@@ -17,6 +17,29 @@ const port = process.env.PORT || 4000
 client.connect(err => {
     const serviceCollection = client.db("ddblog").collection("blogs");
 
+
+    app.post('/addBlog', (req,res) => {
+      const blog = req.body;
+      serviceCollection.insertOne(blog)
+      .then(result => {
+          console.log(result);
+          res.send(result.insertedCount > 0)
+      })
+  })
+
+  app.get('/blogs', (req, res) => {
+    serviceCollection.find({})
+    .toArray((err, documents) => {
+        res.send(documents);
+    })
+  })
+
+  app.get('/blog/:id', (req, res) => {
+    serviceCollection.find({_id: objectId(req.params.id)})
+    .toArray((err, documents) => {
+      res.send(documents[0]);
+    })
+  })
 })
 
 app.get('/', (req, res) => {
